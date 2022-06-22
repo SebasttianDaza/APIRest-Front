6e4a6be96b2a37cc0b06f95c46a38cb8f4cca614
Sebastian Daza <jsebastiandaza2002@gmail.com>
2022-06-21 22:51:37 -0500

    Creation DataBase sql
    I've made APIRest mysql for database

--- DataBase/APIRest.sql
+++ DataBase/APIRest.sql
@@ -8,7 +8,7 @@ USE `APIRest`;
 --- Table Principal
 
 CREATE TABLE `Embarcaciones` (
-    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `name` varchar(45) NOT NULL,
     `country` varchar(45) NOT NULL,
     `continent` varchar(45) NOT NULL,
@@ -46,13 +46,67 @@ INSERT INTO `Embarcaciones` (
 ( NULL, 'El Nuevo Día', 'Colombia', 'South America','23,23'),
 ( NULL, 'Globant', 'Colombia', 'South America','24,24');
 
+-- ID Embarcaciones 1-24 
 
 -- Archicteture Table `usuarios` -- Duenos de las Embarcaciones
 
 CREATE TABLE IF NOT EXISTS `usuarios` (
-    `userID` int(11) NOT NULL AUTO_INCREMENT,
+    `userID` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `userName` varchar(45) NOT NULL,
+    'lastName' varchar(45) NOT NULL,
     'email' varchar(45) NOT NULL,
-    'nameEmbarcacion' KEY (`nameEmbarcacion`),
-    PRIMARY KEY (`userID`)
-)
+    'embarcacionesID' int(11) NOT NULL,
+    FOREIGN KEY (embarcacionesID) REFERENCES Embarcaciones(id),
+    CONSTRAINT UNIQUE (email)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+INSERT INTO `usuarios` (
+    `userID`,
+    `userName`,
+    `lastName`,
+    `email`,
+    `embarcacionesID`
+) VALUES ( NULL, 'Juan', 'Daza', 'jsebastian@gmail.com', 1),
+( NULL, 'Pedro', 'Rodriguez', 'pedro@hotmail.com', 2),
+( NULL, 'Maria', 'Perez',  'mariaagilar12@hotmail.com', 3),
+(NULL, 'Daniel', 'Gonzalez', 'danielgonzales@gmail.com', 4),
+(NULL, 'Esteban', 'Castillo', 'estcas@email.com', 17),
+(NULL, 'Cristian', 'Parra', 'cristianpara@hotmail.com', 18),
+(NULL, 'Daniela', 'Ruiz', 'danielaruiz456@asyrys.com', 17),
+(NULL, 'Daniela', 'Rodriguez', 'danirodriQAdww@hotmail.com', 9 ),
+(NULL, 'Jully', 'Falla', 'jullyfallita@google.com.co', 10),
+(NULL, 'Jessica', 'Marina', 'marinajessica@cargonfive.com', 14),
+(NULL, 'Esperanza', 'Gomez', 'esperanza@cargonfive.com', 13),
+(NULL, 'Geraldine', 'Gomez', 'geraldinelove@gmail.com', 12),
+(NULL, 'Estefany', 'Rhoades', 'rhoadeseste@emprendeyourlifestyle.com', 11),
+(NULL, 'Julian', 'Villalba', 'julianvillalba@cargonfive.com', 15),
+(NULL, 'Harold', 'Arias', 'haroldelsena@sena.com', 16),
+(NULL, 'Martin', 'Velandia', 'martinfhjdsjf12@space.com', 21),
+(NULL, 'Sonia', 'Marsella', 'soniamarsella@tesla.com', 20),
+(NULL, 'Sandra', 'Gonzalez', 'gonzas@gmail.com', 23);
+
+-- ID Embarcaciones 1-18
+
+-- Archicteture Table `sale` -- Ventas de las embarcaciones
+
+CREATE TABLE IF NOT EXISTS `sale` (
+    `saleID` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `embarcacionesID` varchar(45) NOT NULL,
+    `quantity` int(25) NOT NULL,
+    FOREIGN KEY (embarcacionesID) REFERENCES Embarcaciones(id),
+    FOREIGN KEY (userID) REFERENCES usuarios(userID),
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+INSERT INTO `sale` (
+    `saleID`
+    `embarcacionesID`
+    `quantity`
+) VALUES ( NULL, 1, 5),
+( NULL, 2, 10),
+( NULL, 4, 12),
+( NULL, 3, 3),
+( NULL, 5, 12),
+( NULL, 7, 16),
+( NULL, 22, 13),
+( NULL, 19, 2),
+( NULL, 20, 30 );
