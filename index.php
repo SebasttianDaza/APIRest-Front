eacaa2025fc302161e74729e2d2d4c85663c0927
Sebastian Daza <jsebastiandaza2002@gmail.com>
2022-07-01 23:46:17 -0500

    Change archicteture

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    include_once "./Class/Connection/connection.php";
-
-    $connection = new Connection();
-
-    $query = "INSERT INTO Embarcaciones (id, name, country, continent, coordinates) VALUES (NULL, 'Barco1', 'España', 'Europa', '1,1')";
-    
-    //print_r($connection->anyQueryID($query));
-?>
