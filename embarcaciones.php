7b12e79eb164e40e1bec9b9cbe768d1b5b52018b
Sebastian Daza <jsebastiandaza2002@gmail.com>
2022-07-14 22:38:49 -0500

    (feat): Added CORS and DATABASE REMOTE
    Added the cors every route, push remote database and connection with that

--- embarcaciones.php
+++ embarcaciones.php
@@ -2,62 +2,65 @@
     require_once "./Class/Routes/Request.class.php";
     require_once "./Class/Routes/Embarcaciones.class.php";
 
-    //Desabilitar los corss-origin para evitar errores de CORS
-
     $_Request = new Request();
     $_Embarcaciones = new Embarcaciones();
 
-    
+    $_Request->httpResponseHeader($_SERVER['REQUEST_METHOD']);
+
     if($_SERVER["REQUEST_METHOD"] == "GET") {
-        $_Request->httpResponseCode($_SERVER['REQUEST_METHOD']);
         
         if(isset($_GET["page"])) {
-
-            header("Content-Type: application/json");
+            
             $page = $_GET["page"];
             $result = $_Embarcaciones->getListEmbarcaciones($page);
+            
             $_Request->httpResponseCode(200);
+            
             echo $result;
 
         } else if(isset($_GET["id"])) {
 
-            header("Content-Type: application/json");
             $id = $_GET["id"];
             $result = $_Embarcaciones->getEmbarcacion($id);
+
             $_Request->httpResponseCode(200);
+            
             echo $result;
 
         } else {
             echo $_Request->returnResponseJSON($_Request->error_400());
         }
+        
 
     } else if ($_SERVER["REQUEST_METHOD"] == "POST") {
             
-        header("Content-Type: application/json");
         $postBody = file_get_contents("php://input");
         $result = $_Embarcaciones->createEmbarcacion($postBody);
+
         $_Request->httpResponseCode(200);
+
         echo $result;
 
     } else if ($_SERVER["REQUEST_METHOD"] == "PUT") {
 
-        header("Content-Type: application/json");
         $putBody = file_get_contents("php://input");
         $result = $_Embarcaciones->updateEmbarcacion($putBody);
+
         $_Request->httpResponseCode(200);
+
         echo $result;
         
     } else if ($_SERVER["REQUEST_METHOD"] == "DELETE") {
 
-        header("Content-Type: application/json");
         $deleteBody = file_get_contents("php://input");
         $result = $_Embarcaciones->deleteEmbarcacion($deleteBody);
+
         $_Request->httpResponseCode(200);
+
         echo $result;
 
     } else {
 
-        header("Content-Type: application/json");
         $_Request->httpResponseCode(405);
         echo $_Request->returnResponseJSON($_Request->error_405());
     }
--- users.php
+++ users.php
@@ -4,13 +4,13 @@
 
     $_Request = new Request();
     $_Users = new Users();
+    
+    $_Request->httpResponseHeader($_SERVER['REQUEST_METHOD']);
 
     if($_SERVER["REQUEST_METHOD"] == "GET") {
-         $_Request->httpResponseCode($_SERVER['REQUEST_METHOD']);
 
         if(isset($_GET["page"])) {
 
-            header("Content-Type: application/json");
             $page = $_GET["page"];
             $result = $_Users->getList($page);
             $_Request->httpResponseCode(200);
@@ -20,7 +20,6 @@
         
         if(isset($_GET["id"])) {
 
-            header("Content-Type: application/json");
             $id = $_GET["id"];
             $result = $_Users->getUser($id);
             $_Request->httpResponseCode(200);
@@ -35,8 +34,7 @@
     } 
     
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
-            
-        header("Content-Type: application/json");
+        
         $postBody = file_get_contents("php://input");
         $result = $_Users->post($postBody);
         $_Request->httpResponseCode(200);
@@ -45,8 +43,6 @@
     } 
     
     if ($_SERVER["REQUEST_METHOD"] == "PUT") {
-
-        header("Content-Type: application/json");
         $putBody = file_get_contents("php://input");
         $result = $_Users->put($putBody);
         $_Request->httpResponseCode(200);
@@ -55,8 +51,6 @@
     } 
     
     if ($_SERVER["REQUEST_METHOD"] == "DELETE") {
-
-        header("Content-Type: application/json");
         $deleteBody = file_get_contents("php://input");
         $result = $_Users->delete($deleteBody);
         $_Request->httpResponseCode(200);
@@ -65,8 +59,6 @@
     } 
     
     if(!isset($_SERVER["REQUEST_METHOD"])) {
-
-        header("Content-Type: application/json");
         $_Request->httpResponseCode(405);
         echo $_Request->returnResponseJSON($_Request->error_405());
     }
